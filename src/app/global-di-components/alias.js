dc00c8f87d825e4c18a23f9a547d980d22ed1bc4
Vincenzo <vincenzo.villani@studio.unibo.it>
2023-07-12 23:14:18 +0200

    Updates

--- src/app/global-di-components/alias.js
+++
@@ -1,8 +0,0 @@
-import { ComponentsRegistry } from "cle.js/lib";
-
-ComponentsRegistry.define({ navbar: {} }, 'div')
-ComponentsRegistry.define({ contents: {} }, 'div')
-ComponentsRegistry.define({ content: {} }, 'details')
-ComponentsRegistry.define({ channel: {} }, 'div')
-ComponentsRegistry.define({ channel_group: {} }, 'div')
-ComponentsRegistry.define({ footer: {} }, 'div')
--- src/utils/csz.js
+++
@@ -1,5 +0,0 @@
-
-import csz from 'csz'
-
-// fix csz to avoid to use template literal and setup global
-window.css = (v)=>csz([v]) 
